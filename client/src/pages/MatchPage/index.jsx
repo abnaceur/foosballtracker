4927574d6e4e0069623ada3a70c00dc3ddf8373f
abdeljalil Naceur <abdeljalil.naceur@cyconia.io>
2021-03-09 17:31:01 +0100

    Integrate Match page.

--- client/src/pages/MatchPage/index.jsx
+++ client/src/pages/MatchPage/index.jsx
@@ -1,16 +1,49 @@
+import { Title } from '@material-ui/icons';
 import React from 'react';
 
 // Layout
 import MatchPageBaseLayout from '../../layouts/MatchPageBaseLayout';
+import PersonIcon from '@material-ui/icons/Person';
 
 // Styles
-import { Container } from './style';
+import {
+  Container,
+  Header,
+  InfoPos,
+  Team1,
+  Team2,
+  PlayersList,
+  PlayerItem,
+  PlInfo,
+  Teams
+} from './style';
 
 const MatchPage = () => {
   return (
     <MatchPageBaseLayout id='dashboardLayout' className='dashboardLayout'>
       <Container>
-       <h1>MatchPage</h1>
+
+        {/* Header */}
+        <Header>
+          <InfoPos> Defense + Offense </InfoPos>
+          <Team1>Team 1 : aaaa + cccccc</Team1>
+          <Team2>Team 2 : aaaa + cccccc</Team2>
+        </Header>
+
+        <PlayersList>
+          <InfoPos>Players List</InfoPos>
+          <PlayerItem>
+            <PlInfo>
+              <PersonIcon style={{fontSize: "14px"}} /> Abdeljalil
+          </PlInfo>
+            <Teams>
+              <span>Team1</span>
+              <span>Team2</span>
+            </Teams>
+          </PlayerItem>
+
+        </PlayersList>
+
       </Container>
     </MatchPageBaseLayout>
   );
--- client/src/pages/MatchPage/style.js
+++ client/src/pages/MatchPage/style.js
@@ -3,4 +3,62 @@ import styled from "styled-components";
 export const Container = styled.div`
   flex: 1;
   min-height: 100vh;
+  padding: 40px;
 `;
+
+export const Header = styled.div`
+
+`
+
+export const InfoPos = styled.div`
+  font-size: 28px;
+  font-weight: bold;
+  padding-left: 100px;
+`
+export const Team1 = styled.div`
+  font-size: 26px;
+  font-weight: bold;
+`
+
+export const Team2 = styled.div`
+  font-size: 26px;
+  font-weight: bold;
+
+`
+export const PlayersList = styled.div`
+  display: flex;
+  padding-top: 50px;
+  flex-direction: column;
+`
+export const PlayerItem = styled.span`
+  margin-top: 40px;
+  width: max-content;
+  display:flex;
+  flex-direction: column;
+
+`
+export const PlInfo = styled.span`
+border-radius: 5px;
+border: 1px solid black;
+padding: 10px 20px;
+
+`
+export const Teams = styled.span`
+  display: flex:
+  justify-content: space-between;
+
+  span {
+    border-radius: 5px;
+    border: 1px solid black;
+    padding: 4px 8px;
+    margin-right: 10px;
+    cursor: pointer;
+    background-color: #2196F3;  
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    &:hover {
+      background-color: black;    
+    }
+  }
+`
