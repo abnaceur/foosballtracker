5c9a14241f5195caf2c0dedc8ae27e85f3f117aa
abdeljalil Naceur <abdeljalil.naceur@cyconia.io>
2021-03-09 21:14:33 +0100

    Integrate Setting page.

--- client/src/pages/SettingsPage/index.jsx
+++ client/src/pages/SettingsPage/index.jsx
@@ -1,16 +1,55 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 // Layout
 import SettingPageBaseLayout from '../../layouts/SettingPageBaseLayout';
+import PlayersListItems from './components/PlayersList';
 
 // Styles
-import { Container } from './style';
+import {
+  Container,
+  PlayersList
+} from './style';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
 
 const SettingPage = () => {
+  const classes = useStyles();
+
   return (
     <SettingPageBaseLayout id='dashboardLayout' className='dashboardLayout'>
       <Container>
-       <h1>SettingPage Page</h1>
+        <h1>Add new player</h1>
+        <TextField
+          placeholder="Add a new player ..."
+          className={classes.margin}
+          id="input-with-icon-textfield"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button variant="contained" color="primary">
+          Add
+      </Button>
+
+        <PlayersList>
+          <h1>Players list</h1>
+          <PlayersListItems />
+        </PlayersList>
+
       </Container>
     </SettingPageBaseLayout>
   );
--- client/src/pages/SettingsPage/style.js
+++ client/src/pages/SettingsPage/style.js
@@ -3,4 +3,18 @@ import styled from "styled-components";
 export const Container = styled.div`
   flex: 1;
   min-height: 100vh;
+  padding: 40px
 `;
+
+export const AddRow = styled.div`
+  display: flex;
+  span {
+    font-size: 28px;
+    font-weight: 600;
+  }
+`
+export const PlayersList = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+`
