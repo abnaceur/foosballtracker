e2fdb7b838ccc60b0e0a074d1610d1c607a075da
abdeljalil Naceur <abdeljalil.naceur@cyconia.io>
2021-03-13 20:31:37 +0100

    Create database tables.

--- server/config/configDatabase.js
+++ server/config/configDatabase.js
@@ -1,6 +1,6 @@
 databaseTableList = () => {
     return [{
-        name: "Player",
+        name: "player",
         table: "CREATE TABLE IF NOT EXISTS `player` ( \
             `id` INT NOT NULL AUTO_INCREMENT, \
             `name` VARCHAR(90) NOT NULL, \
@@ -8,8 +8,41 @@ databaseTableList = () => {
             `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \
             `deletedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \
             PRIMARY KEY (`id`),UNIQUE INDEX `iduser_UNIQUE` (`id` ASC))"
-    }
-    ];
+    },
+    {
+        name: "game",
+        table: "CREATE TABLE IF NOT EXISTS `game` ( \
+            `id` INT NOT NULL AUTO_INCREMENT, \
+            `team1Id` INT NOT NULL, \
+            `team2Id` INT NOT NULL, \
+            `winner` INT NOT NULL, \
+            `duration` INT NOT NULL, \
+            `startedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+            `finishedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+            PRIMARY KEY (`id`),UNIQUE INDEX `idgame_UNIQUE` (`id` ASC))"
+    },
+    {
+        name: "team",
+        table: "CREATE TABLE IF NOT EXISTS `team` ( \
+            `id` INT NOT NULL AUTO_INCREMENT, \
+            `player1Id` INT NOT NULL, \
+            `player2Id` INT NOT NULL, \
+            `player1pos` VARCHAR(90) NOT NULL, \
+            `player2pos` VARCHAR(90) NOT NULL, \
+            `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+            PRIMARY KEY (`id`),UNIQUE INDEX `idteam_UNIQUE` (`id` ASC))"
+    },
+    {
+        name: "score",
+        table: "CREATE TABLE IF NOT EXISTS `score` ( \
+            `id` INT NOT NULL AUTO_INCREMENT, \
+            `gameId` INT NOT NULL, \
+            `pl1Goals` INT NOT NULL, \
+            `pl2Goals` INT NOT NULL, \
+            `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \
+            PRIMARY KEY (`id`),UNIQUE INDEX `idscore_UNIQUE` (`id` ASC))"
+    },
+    ]; 
 }
 
 module.exports = {
